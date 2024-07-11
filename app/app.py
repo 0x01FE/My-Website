c3f7cd5ac7569ee070cb5b6ee00960bb1c6597b5
0x01fe <jackson.d.hodge@jbhunt.com>
2024-07-11 15:35:23 -0500

    forgot to remove the endpoint

--- app/app.py
+++ app/app.py
@@ -231,27 +231,6 @@ def programming():
 
     return flask.render_template('programming.html', posts=posts_and_comments, form=form, user=user, status=status)
 
-@app.route('/writing/')
-def writing():
-
-    works = []
-
-    # Get all works in writing folder
-    files = glob.glob(WRITING_FOLDER + '*')
-
-    for path in files:
-
-        date: str = datetime.datetime.fromtimestamp(os.path.getctime(path)).strftime("%B %d, %Y")
-        name: str = path.split('/')[-1]
-
-        works.append({
-            'date' : date,
-            'name' : name,
-            'path' : path
-        })
-
-    return flask.render_template('writing.html', works=works)
-
 # About Page
 @app.route('/about/')
 def about():
